3ab4ae0fe99c027bff52ab847e7d919743df0359
DanioFiore <daniofiore@hotmail.com>
2022-10-01 17:48:28 +0200

    added filter by date component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,8 @@
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
 import "./Expenses.css";
 import ExpenseForm from "../ExpenseForm/ExpenseForm";
+import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
+import React, {useState} from "react";
 
 const Expenses = (props) => {
     const onSaveExpenseDataHandler = (enteredExpenseData) => {
@@ -12,11 +14,22 @@ const Expenses = (props) => {
         
     }
 
+    const [filterDate, setFilterDate] = useState('2022');
+    const filterChangeHandler = (selectedYear) => {
+        setFilterDate(selectedYear)
+        
+    }
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filterDate;
+    })
+
     return (
         <div>
             <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+            <ExpenseFilter onFilterChange={filterChangeHandler} startDate={filterDate}/>
             <div className="expenses-container">
-                {props.items.map((element) => (<ExpenseItem key={element.id} title={element.title} amount={element.amount} date={element.date} />))}
+                {filteredExpenses.map((element) => (<ExpenseItem key={element.id} title={element.title} amount={element.amount} date={element.date} />))}
             </div>
         </div>
 
